66fb94d90a85493c166d76dd78c31e9bf5e8cc05
Briaulin Mendez <briaulinmendez@MacBook-Pro.home>
2022-05-03 01:41:20 +0200

    Configuración de la conexión

---
+++ database/db.ts
@@ -0,0 +1,40 @@
+import mongoose from "mongoose";
+
+/**
+ * 0 = disconnected
+ * 1 = connected
+ * 2 = connecting
+ * 3 = disconnecting
+ */
+const mongoConnection = {
+  isConnected: 0,
+};
+
+export const connect = async () => {
+  if (mongoConnection.isConnected) {
+    console.log("Ya estábamos conectados a mongodb");
+    return;
+  }
+
+  if (mongoose.connections.length > 0) {
+    mongoConnection.isConnected = mongoose.connections[0].readyState;
+    if (mongoConnection.isConnected === 1) {
+      console.log("Usando conexión anterior");
+      return;
+    }
+
+    await mongoose.disconnect();
+  }
+
+  await mongoose.connect(process.env.MONGO_URL || "");
+  mongoConnection.isConnected = 1;
+  console.log("Conectado a mongodb", process.env.MONGO_URL);
+};
+
+export const disconnect = async () => {
+  if (mongoConnection.isConnected === 0) return;
+
+  await mongoose.disconnect();
+  mongoConnection.isConnected = 0;
+  console.log("Desconectado de mongo");
+};
